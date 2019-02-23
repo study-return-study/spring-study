e26f24f9f5c94b8f0ba198069a8ffb9fbd786645
BreadKey <t9rres@ajou.ac.kr>
2019-02-24 00:19:57 +0900

    service에 dao autowire하고 사용해보기

--- study-dataaccess/breadkey/src/datasource/pokedex/Main.java
+++ study-dataaccess/breadkey/src/datasource/pokedex/Main.java
@@ -4,6 +4,7 @@ import datasource.pokedex.config.AppConfig;
 import datasource.pokedex.config.DataSourceConfig;
 import datasource.pokedex.config.TemplateConfig;
 import di_bean.pokedex.di.business.domain.Pokemon;
+import di_bean.pokedex.di.business.service.PokedexService;
 import di_bean.pokedex.di.dataaccess.PokemonDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -22,13 +23,14 @@ public class Main {
         String pokemonName = jdbcTemplate.queryForObject("SELECT POKEMON_NAME FROM POKEMON WHERE POKEMON_ID=?", String.class, id);
         System.out.println(pokemonName);
 
-        PokemonDao pokemonDao = context.getBean(PokemonDao.class);
-        Pokemon pikachu = pokemonDao.findByPokemonId(id);
+        PokedexService pokedexService = context.getBean(PokedexService.class);
+        Pokemon pikachu = pokedexService.findByPokemonId(id);
         System.out.println(pikachu.getName());
         System.out.println("공격: " + pikachu.getAttack());
         System.out.println("방어: " + pikachu.getDefense());
         System.out.println("특수공격: " + pikachu.getSpecialAttack());
         System.out.println("특수방어: " + pikachu.getSpecialDefense());
         System.out.println("스피드: " + pikachu.getSpeed());
+        System.out.println("종족값 총합: " + pokedexService.calculateTotalBaseStats(pikachu));
     }
 }
--- study-dataaccess/breadkey/src/datasource/pokedex/config/AppConfig.java
+++ study-dataaccess/breadkey/src/datasource/pokedex/config/AppConfig.java
@@ -1,7 +1,10 @@
 package datasource.pokedex.config;
 
 import datasource.pokedex.dataaccess.PokemonDaoImpl;
+import di_bean.pokedex.di.business.service.PokedexService;
+import di_bean.pokedex.di.business.service.PokedexServiceImpl;
 import di_bean.pokedex.di.dataaccess.PokemonDao;
+import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +13,10 @@ public class AppConfig {
     @Bean
     public PokemonDao pokemonDao() {
         return new PokemonDaoImpl();
+    }
+
+    @Bean(autowire = Autowire.BY_TYPE)
+    public PokedexService pokedexService() {
+        return new PokedexServiceImpl();
     }
 }
